073234788c2696ccf115c3211500b697b11832b1
SunnySideUp <chanjung4505@gmail.com>
2023-04-27 20:05:28 +0900

    Belive_This? 프로젝트
    스프링 부트 프로젝트 초기화

---
+++ src/main/java/com/example/belivethis/BeliveThisApplication.java
@@ -0,0 +1,13 @@
+package com.example.belivethis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeliveThisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeliveThisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/belivethis/BeliveThisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.belivethis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeliveThisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
